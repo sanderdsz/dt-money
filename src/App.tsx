6d11784a6abed6adb4c400293e3ffff8fe77b05b
sanderdsz <sanderdsz@gmail.com>
2021-12-27 21:20:40 -0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyle } from './styles/global'
+import { Header } from './components/Header'
 
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <GlobalStyle />
-      <h1>hello</h1>
-    </div>
+    </>
   );
 }
