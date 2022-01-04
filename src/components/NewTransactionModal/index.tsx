7e94a86b49086d76a0e2500e9f6b3c8940e5946c
sanderdsz <sanderdsz@gmail.com>
2022-01-04 20:49:36 -0300

    Using provider to register

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,9 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, useContext } from 'react'
 import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 
 Modal.setAppElement('#root')
 
@@ -14,15 +15,27 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const  { createTransaction } = useContext(TransactionsContext)
+
   const [type, setType] = useState('deposit')
   const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0)
+  const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState('')
 
   function handleCreateNewTransaction(event: FormEvent) {
-    event.preventDefault()
-    const data = {type, title, value, category}
-    api.post('transactions', data)
+    event.preventDefault();
+
+    createTransaction({
+      title,
+      amount,
+      category,
+      type
+    });
+    setType('deposit');
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    onRequestClose();
   }
 
   return(
@@ -53,8 +66,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             <input 
               type="number" 
               placeholder="Value" 
-              value={value}
-              onChange={event => setValue(Number(event.target.value))} 
+              value={amount}
+              onChange={event => setAmount(Number(event.target.value))} 
             />
             <TransactionTypeContainer>
               <RadioBox 
